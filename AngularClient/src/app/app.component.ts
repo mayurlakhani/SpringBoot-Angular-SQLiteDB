8d0f57f19e4cff1dba499cd2b82ba42d2f4b1117
MayurLakhani <mayur22lakhani33@gmail.com>
2020-04-22 22:10:40 +0200

    final commit

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -10,5 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit  {
 
 ngOnInit() {}
+
+ navBtn() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 }
 
